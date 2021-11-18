4cb2a08f175df95e7bf6021bf711dad0c72c3b20
aurelappius <appiusa@student.ethz.ch>
2021-11-18 16:59:49 +0100

    put run and close into functions

--- main.cpp
+++ main.cpp
@@ -2,54 +2,36 @@
 #include <stdlib.h>
 #include <string>
 
+//const char pwd[] = parameters::user;
 const char pwd[] = "raspberry";
 const char usr[] = "pi@10.4.51.118";
-const char cmd[] = "ls";
+const char cmd[] = "python hello.py";
+const char shutdown_cmd[] = "sudo shutdown now";
 
-int run_ssh_cmd(ssh_session session, const char command[]);
+int run_ssh_cmd(ssh_session& session, const char command[]);
+
+int open_ssh(ssh_session& session, const char user[], const char password[]);
+
+int close_ssh(ssh_session& session);
 
 int main()
 {
-  //declaring session
   ssh_session my_ssh_session;
   int rc;
- 
-  // Open session and set options
-  my_ssh_session = ssh_new();
-  if (my_ssh_session == NULL)
-    exit(-1);
-  ssh_options_set(my_ssh_session, SSH_OPTIONS_HOST, usr);
- 
-  // Connect to server
-  rc = ssh_connect(my_ssh_session);
-  if (rc != SSH_OK)
-  {
-    fprintf(stderr, "Error connecting to localhost: %s\n",
-            ssh_get_error(my_ssh_session));
-    ssh_free(my_ssh_session);
-    exit(-1);
-  }
- 
-  // Authenticate ourselves
-  rc = ssh_userauth_password(my_ssh_session, NULL, pwd);
-  if (rc != SSH_AUTH_SUCCESS)
-  {
-    fprintf(stderr, "Error authenticating with password: %s\n",
-            ssh_get_error(my_ssh_session));
-    ssh_disconnect(my_ssh_session);
-    ssh_free(my_ssh_session);
-    exit(-1);
-  }
- 
+
+  //opening ssh connection
+  open_ssh(my_ssh_session, usr, pwd);
   //running the command
   run_ssh_cmd(my_ssh_session, cmd);
- 
-  //disconnecting from ssh session
-  ssh_disconnect(my_ssh_session);
-  ssh_free(my_ssh_session);
+  
+  //shutdown the pi
+  //run_ssh_cmd(my_ssh_session, shutdown_cmd);
+
+  //closing ssh_connection
+  close_ssh(my_ssh_session);
 }
 
-int run_ssh_cmd(ssh_session session, const char command[]){
+int run_ssh_cmd(ssh_session& session, const char command[]){
   ssh_channel channel;
   int rc;
  
@@ -96,4 +78,43 @@ int run_ssh_cmd(ssh_session session, const char command[]){
   ssh_channel_free(channel);
  
   return SSH_OK;
+}
+
+int open_ssh(ssh_session& session, const char user[], const char password[]){
+  int rc;
+ 
+  // Open session and set options
+  session = ssh_new();
+  if (session == NULL)
+    exit(-1);
+  ssh_options_set(session, SSH_OPTIONS_HOST, user);
+ 
+  // Connect to server
+  rc = ssh_connect(session);
+  if (rc != SSH_OK)
+  {
+    fprintf(stderr, "Error connecting to localhost: %s\n",
+            ssh_get_error(session));
+    ssh_free(session);
+    exit(-1);
+  }
+ 
+  // Authenticate ourselves
+  rc = ssh_userauth_password(session, NULL, password);
+  if (rc != SSH_AUTH_SUCCESS)
+  {
+    fprintf(stderr, "Error authenticating with password: %s\n",
+            ssh_get_error(session));
+    ssh_disconnect(session);
+    ssh_free(session);
+    exit(-1);
+  }
+ return 0;
+}
+
+
+int close_ssh(ssh_session& session){
+  ssh_disconnect(session);
+  ssh_free(session);
+  return 0;
 }
